d476b976eb05c2979bce8502d4a5772da1ed97fa
xFeWo <x_feWo_x@gmx.de>
2025-02-14 22:00:57 +0100

    added datetime and connectivity

---
+++ script.js
@@ -0,0 +1,16 @@
+function updateDateTime() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+    const weekday = now.toLocaleDateString('de-DE', { weekday: 'long' });
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = now.toLocaleDateString('de-DE', { month: 'short' }).replace('.', '');
+    const formattedDateTime = `${time} &nbsp; ${weekday} ${day}. ${month}.`;
+
+    document.getElementById("datetime").innerHTML = formattedDateTime;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+});
