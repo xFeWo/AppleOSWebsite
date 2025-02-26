741892c8147443e13b3031e23935804cd538ed78
xFeWo <x_feWo_x@gmx.de>
2025-02-26 20:52:19 +0100

    improved control center interactivity

--- script.js
+++ script.js
@@ -2,31 +2,26 @@ function openControlCenter() {
     var controlCenter = document.getElementById("control-center");
     var overlay = document.getElementById("overlay");
     var menuIcon = document.querySelector(".open-control-center");
+    var searchIcon = document.querySelector(".open-search-bar")
 
     controlCenter.classList.toggle("toggle-control-center");
     overlay.classList.add("show");
 
     menuIcon.classList.add("hidden");
+    searchIcon.classList.add("hidden");
 }
 
 function closeControlCenter() {
     var controlCenter = document.getElementById("control-center");
     var overlay = document.getElementById("overlay");
     var menuIcon = document.querySelector(".open-control-center");
+    var searchIcon = document.querySelector(".open-search-bar")
 
     controlCenter.classList.remove("toggle-control-center");
     overlay.classList.remove("show");
 
     menuIcon.classList.remove("hidden");
-}
-
-function toggleImage() {
-    var img = document.getElementById("playPauseButton");
-    if (img.src.includes("play.png")) {
-        img.src = "images/control_center/mediaplayer/pause.png";
-    } else {
-        img.src = "images/control_center/mediaplayer/play.png";
-    }
+    searchIcon.classList.remove("hidden");
 }
 
 function updateDateTime() {
@@ -45,3 +40,50 @@ document.addEventListener("DOMContentLoaded", function () {
     updateDateTime();
     setInterval(updateDateTime, 1000);
 });
+
+var audio = new Audio('music/you_spin_me_round.mp3');
+
+function togglePlayPauseButton() {
+    var img = document.getElementById("play-pause-button");
+    if (img.src.includes("play.png")) {
+        img.src = "images/control_center/mediaplayer/pause.png";
+        audio.play();
+    } else {
+        img.src = "images/control_center/mediaplayer/play.png";
+        audio.pause();
+    }
+}
+
+function togglePlayPause() {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+function toggleBellButton() {
+    var img = document.getElementById("bell-button");
+    if (img.src.includes("bell_on.png")) {
+        img.src = "images/control_center/bell_off.png";
+        img.classList.toggle("control-center-bell-icon-on");
+        img.classList.toggle("control-center-bell-icon-off");
+    } else {
+        img.src = "images/control_center/bell_on.png";
+        img.classList.toggle("control-center-bell-icon-on");
+        img.classList.toggle("control-center-bell-icon-off");
+    }
+}
+
+function toggleLockButton() {
+    var img = document.getElementById("lock-button");
+    if (img.src.includes("lock_on.png")) {
+        img.src = "images/control_center/lock_off.png";
+        img.classList.toggle("control-center-lock-icon-on");
+        img.classList.toggle("control-center-lock-icon-off");
+    } else {
+        img.src = "images/control_center/lock_on.png";
+        img.classList.toggle("control-center-lock-icon-on");
+        img.classList.toggle("control-center-lock-icon-off");
+    }
+}
